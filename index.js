1a890d58187f304745443816bdf8117b7a09f9d3
xcatliu <xcatliu@gmail.com>
2016-02-05 00:25:55 +0800

    Add index.html

---
+++ index.js
@@ -0,0 +1,19 @@
+/* global $ */
+/* eslint no-use-before-define:0 */
+
+$(function readyFn() {
+  // $('body').css({
+  //   'background-color':
+  // })
+  // var windowHeight = $(window).height();
+  // alert(windowHeight);
+  // setWordsFontHeight();
+  //
+  // function setWordsFontHeight() {
+  //   $('.words').css({
+  //     'font-size': windowHeight + 'px',
+  //     'line-height': windowHeight + 'px',
+  //     width: 999999999 + 'px',
+  //   });
+  // }
+});
